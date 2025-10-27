e847125c648aece19b676e97c92258da1f34dd5f
Alba Muñoz <albamu2002@gmail.com>
2025-10-27 18:52:54 +0100

    Add files via upload

---
+++ python-rest/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/item")
+async def item_post(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.get("/item")
+async def get_item(item: Item):
+    item_dict = item.model_dump()
+    return item_dict
+
+@app.get("/item/{item_id}")
+async def get_item(item_id: int):
+    if item_id == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item_id": item_id}
