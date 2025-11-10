9cb2fb762b42dc7a261f91f51a44fc4a2ec48bc3
alba-mu <albamu2002@gmail.com>
2025-11-10 17:07:41 +0100

    Refactor create_item(), get_all_items() and get_item_by_id() to adjust to sample db logic

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
 
 app = FastAPI()
 
+# Model Pydantic
 class Item(BaseModel):
     id: int
     name: str
@@ -10,31 +13,34 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+# Base de dades simulada
 db: dict[int, Item] = {
     1: Item(id=1, name="Laptop", description="A high-performance laptop for development", price=100.99),
     2: Item(id=2, name="Mouse", description="Wireless ergonomic mouse", price=15.95),
     3: Item(id=3, name="Keyboard", price=23.45, tax=3.54),
 }
 
-@app.post("/item")
-async def item_post(item: Item):
-    item_dict = item.model_dump()
-    if item_dict.id in db:
+
+# Crear un nou item
+@app.post("/item", response_model=Item)
+async def create_item(item: Item):
+    if item.id in db:
         raise HTTPException(status_code=409, detail="Item already exists")
-    if item_dict.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    db[item_dict.id] = item_dict
-    return item_dict
+
+    db[item.id] = item
+    return item
 
 
-@app.get("/item")
-async def get_item(item: Item):
-    item_dict = item.model_dump()
-    return item_dict
+# Obtenir tots els items
+@app.get("/item", response_model=list[Item])
+async def get_all_items():
+    return list(db.values())
 
-@app.get("/item/{item_id}")
-async def get_item(item_id: int):
-    if item_id == 0:
+
+# Obtenir un item per id
+@app.get("/item/{item_id}", response_model=Item)
+async def get_item_by_id(item_id: int):
+    if item_id not in db:
         raise HTTPException(status_code=404, detail="Item not found")
-    return {"item_id": item_id}
+
+    return db[item_id]
